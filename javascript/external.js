61453b0ac8e74eaee6100299d1bf9e6f40b149ab
emanley24225 <emanley24225@gmail.com>
2021-09-26 18:51:21 -0400

    Completed Task 3. Variables.

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,15 @@
 // window.alert(5 + 6);
-document.write('<h1> A heading </h1>');
-document.write('<p> A senctence </p>');
+/* const x = "web"
+const y = "mapping"
+const out = x + y
+document.write(out)*/
+/* A prompt box is used to prompt users to input a value
+before entering a page.*/
+user_name = window.prompt("Please enter your name",
+"Type your name here");
+document.write(user_name)
+// There are many ways to use the prompt feature.
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); // open the blank prompt window
+sign = window.prompt('Are you feeling lucky', 'sure') /* open the window
+with the Text  "Are you feeling lucky" and default answer "sure".*/
