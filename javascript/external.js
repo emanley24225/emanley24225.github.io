f7e2441cae55ec39f90885acb64ff25a4ce50cfd
emanley24225 <emanley24225@gmail.com>
2021-09-26 21:09:48 -0400

    Completed Task 6. Array.

--- javascript/external.js
+++ javascript/external.js
@@ -1,33 +1,38 @@
 // window.alert(5 + 6);
-/* const x = "web"
-const y = "mapping"
-const out = x + y
-document.write(out)*/
-/* A prompt box is used to prompt users to input a value
-before entering a page.
-user_name = window.prompt("Please enter your name",
-"Type your name here");
-document.write(user_name)
+// const x = "web"
+// const y = "mapping"
+// const out = x + y
+// document.write(out)
+// A prompt box is used to prompt users to input a value
+// before entering a page.
+// user_name = window.prompt("Please enter your name",
+// "Type your name here");
+// document.write(user_name)
 // There are many ways to use the prompt feature.
-sign = window.prompt(); // open the blank prompt window
-sign = prompt(); // open the blank prompt window
-sign = window.prompt('Are you feeling lucky', 'sure') /* open the window
-with the Text  "Are you feeling lucky" and default answer "sure".*/
-/* x = 22;
-const y = 33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33); */
-document.writeln("<button onclick ='condition()'>Conditional Test</button>")
-function condition()
+// sign = window.prompt(); // open the blank prompt window
+// sign = prompt(); // open the blank prompt window
+// sign = window.prompt('Are you feeling lucky', 'sure')  open the window
+// with the Text  "Are you feeling lucky" and default answer "sure".
+//  x = 22;
+// const y = 33;
+// document.writeln(x + y);
+// document.writeln("<br>");
+// document.writeln(x += 33);
+// document.writeln("<button onclick ='condition()'>Conditional Test</button>")
+// function condition()
+// {
+//   x = confirm("Are you sure you want to proceed?");
+//   if(x)
+//   {
+//     document.writeln("You choose Okay!");
+//   }
+//   else
+//   {
+//     document.writeln("You chose Cancel!");
+//   }
+// }
+arr = ["Joe","April","Mark","Sara"];
+for (var i=0; i < arr.length; i++)
 {
-  x = confirm("Are you sure you want to proceed?");
-  if(x)
-  {
-    document.writeln("You choose Okay!");
-  }
-  else
-  {
-    document.writeln("You chose Cancel!");
-  }
+ document.writeln(arr[i],"<br>");
 }
