7776cf85a8d73980e3b59d1bd681a890d0513f82
emanley24225 <emanley24225@gmail.com>
2021-10-04 03:06:12 -0400

    Tried to figure out #4.

--- javascript/external.js
+++ javascript/external.js
@@ -34,9 +34,7 @@ function webmap_table()
 {
   document.write("<table width = 100%>");
   for (var row =0; row < webmaps.length; row++)
-  { if (webmaps == 1) {
-    document.table.style.backgroundColor = "red";
-  }
+  {
     document.write("<tr>");
     for (var column=0; column < webmaps[0].length; column++)
     {
